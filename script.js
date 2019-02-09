c76adc764bfe7be4005e8f99d148b0c074bf3db9
Klein <klein.simon@csedge.org>
2019-02-09 15:17:31 -0500

    Website Fixes
    Changes

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-let newHeading = document.createElement("h1");
-newHeading.innerHTML = "FUN!";
-let about = document.getElementById("About-Me")
-about.appendChild(newHeading);
+let about = document.getElementById("About-Me");
+let bodyArray = document.getElementsByTagName("BODY");
+let body = bodyArray[0];
+
+document.addEventListener("click", whenClicked);
+
+about.addEventListener("mouseover", whenHovered);
+about.addEventListener("mouseout", whenDoneHovering);
+
+function whenClicked(){
+	let newHeading = document.createElement("h1");
+	newHeading.innerHTML = "FUN!";
+	newHeading.style.color = "blue";
+	newHeading.style.fontSize = "48px";
+	let about = document.getElementById("About-Me")
+	about.appendChild(newHeading);
+
+}
+
+function whenHovered(){
+	body.style.color = "pink";
+}
+
+function whenDoneHovering(){
+	body.style.color = "white";
+}
